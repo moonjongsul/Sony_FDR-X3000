802e85272fd085594d71ecbc57effbba4a81b400
moonjongsul <moonjongsul@gmail.com>
2022-07-22 23:32:49 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+
+install_requires = [
+    'opencv-python==4.5.5.64'
+]
+
+setup(
+    name='Sony_FDR-X3000',
+    version='0.1',
+    description='Sony FDR-X3000 webcam stream',
+    author='Jongsul Moon',
+    author_email='moonjongsul@keti.re.kr',
+
+)
